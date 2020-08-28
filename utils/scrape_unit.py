14400cfb52aed359d1f71ba0a45c28b3315b2cf4
Josh Wilkinson <joshlwilkinson9@gmail.com>
2020-08-29 00:00:17 +1000

    Begin creating unit object for assessments,

--- utils/scrape_unit.py
+++ utils/scrape_unit.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import sys
-import unit 
+import unit as u
 
 def find_url(unit_code, year, sem_code):
     # Returns the url for a given unit code
     return "https://www.sydney.edu.au/units/" + unit_code + "/" + str(year) + "-" + sem_code.upper() + "-ND-CC"
 
-def get_assessments(unit_url):
+def get_assessments(unit_url,unit):
     # Regex for get_html matches the assessments table on the usyd website
+    # Modifying the unit object by building up the assessment list 
 
-    assessment = unit.Assessment()
 
     try:
         # Read 
@@ -27,20 +27,41 @@ def get_assessments(unit_url):
         raise ValueError("No Assessment Table found")
 
     rows = [row for row in table.itertuples()]
+
+    ls = []
+
     for row in rows:
 
-        print("Type = {}, Desc. = {}, Weight = {}, Due = {}, Length = {}".format(row.Type.split("  ")[0], row.Description.split("  ")[0], row.Weight.split("  ")[0], row.Due.split("  ")[0], row.Length))
-        due_string = row.Due.split("  ")
-        if len(due_string) > 1:# and due_string[1].startswith("Due Date:"):
-            # this is the specific due date for a subject if it is given
-            print(due_string[1][10:])
+        assessment = u.Assessment()
+        assessment.set_unit(unit)
+        assessment.set_type_str(row.Type.split("  ")[0])
 
+        weight = row.Weight.split("  ")[0]
         description = row.Description.split("  ")
-        if len(description) > 1:
-            print("Desc Title = {}".format(description[0]))
-            print("Desc Body = {}".format(description[1]))
+        body = None
 
-def get_schedule(unit_url):
+        if len(description) > 1:
+            body = description[1]
+
+        assessment.set_weight_desc_body(weight, description[0], body = body)
+
+        due_string = row.Due.split("  ")
+        due_date = None
+        if len(due_string) > 1:
+            # this is the specific due date for a subject if it is given
+            due_date = due_string[1][10:]
+        due_string = due_string[0]
+
+        # TODO - implement final exam checking 
+        assessment.set_due_strings(due_string, False, due_date = due_date)
+
+        assessment.set_length(row.Length)
+
+        unit.add_assessment(assessment)
+        # print(assessment)
+
+
+def get_schedule(unit_url, unit):
     # Regex for get_html matches the assessments table on the usyd website
     try:
         tables = pd.read_html(unit_url, match="WK", header = 0)
@@ -52,18 +73,23 @@ def get_schedule(unit_url):
     table = tables[0]
     rows = [row for row in table.itertuples()]
 
-    [print(row) for row in rows]
-    print(tables)
+    # [print(row) for row in rows]
+    # print(tables)
 
 
-def scrape(unit_url):
-    assessments = get_assessments(unit_url)
-    schedule = get_schedule(unit_url)
+def scrape(unit_url, unit):
+    assessments = get_assessments(unit_url, unit)
+    schedule = get_schedule(unit_url,unit)
 
 
 def scrape_unit_obj(unit_code, year = '2020', sem_code = 'S2C'):
+    unit = u.Unit()
+    unit.name = unit_code 
+
     unit_url = find_url(unit_code, year, sem_code)
-    scrape(unit_url)    
+    scrape(unit_url, unit)
+
+    print(unit)
     
     # go to that url
     # create unit object with the name givne
