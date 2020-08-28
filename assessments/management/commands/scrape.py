6df6c71ce3a93f857525d228aaf62d09a093bd5f
Josh Wilkinson <joshlwilkinson9@gmail.com>
2020-08-29 02:04:28 +1000

    Added initial scraping/saving code

--- assessments/management/commands/scrape.py
+++ assessments/management/commands/scrape.py
@@ -1,5 +1,8 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
+import assessments.models
+import utils.find_subj as find
+import utils.scrape_unit as scrape
 
 
 class Command(BaseCommand):
@@ -11,6 +14,23 @@ class Command(BaseCommand):
         time = timezone.now().strftime('%X')
         print('Scraping URLs started at %s\n' % time, end="")
 
+        thing = find.get_all_unit_codes()
+        for ting in thing:
+        	print(ting)
+        	(code, name) = ting.split("&")
+        	try:
+        		scrape.scrape_unit_obj(code, name)
+        	except ValueError as ve:
+        		if str(ve) == "Unit website does not exist":
+        			print("going to next website")
+        			f = open("nonExists.txt", "a")
+        			f.write("{}\n".format(code))
+        			f.close()
+
+        			continue
+        		else:
+        			raise ValueError()
+        	print("Saving unit {}: {} to database".format(code, name))
         # Print time of scraping end to the console
         time = timezone.now().strftime('%X')
         print('\nUnits all saved to database at %s\n' % time, end="")
