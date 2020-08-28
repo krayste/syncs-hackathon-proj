c5cd6ddf5ed4b6044a735587a605071b1ad6fc77
Steve Kraynov <steve.kraynov@gmail.com>
2020-08-28 22:30:57 +1000

    Updated references to parent objects

--- utils/unit.py
+++ utils/unit.py
@@ -23,20 +23,24 @@ class Unit:
 
 class Assessment:
     def __init__(self):
-        # Reference to a unit object
+        # Reference to a unit object 
         self.unit = None
 
         self.type_str = None
         self.description_title = None
         self.description_body = None
 
-        self.weight = None
+        self.weight = None 
 
         # Due str, ie "Multiple Weeks"
         self.due_str = None
-        self.due_date = None
+        self.due_date = None 
         self.is_final = False
-        self.length = None
+        self.length = None 
+
+    def set_unit(self, unit):
+        ''' Set reference back to unit object parent '''
+        self.unit = unit
 
     def set_weight_desc_body(self, weight, description, body=None):
         ''' Setter method for description and weight strings'''
@@ -44,15 +48,56 @@ class Assessment:
         self.description_body = description
         self.description_body = body
 
-    def set_due_strings(self, due_str, is_final, due_date=None):
+    def set_due_strings(self, due_str, is_final, due_date = None):
         ''' Setter method for the due strings '''
         self.due_str = due_str
         self.is_final = is_final
         self.due_date = due_date
-
+    
     def set_length(self, length):
         ''' Sets the length string attribute '''
-        self.length = length
+        self.length = length 
+
+class Schedule:
+    def __init__(self):
+        self.unit = None
+
+        self.wk_str = None
+        self.list_of_topics = []
+
+    def set_unit(self, unit):
+        ''' Set reference back to unit object parent '''
+        self.unit = unit
+
+    def set_week(self, week):
+        self.wk_str = week
+
+    def add_topic(self, topic):
+        ''' Appends a topic object to the list of topics '''
+        self.list_of_topics.append(topic)
+
+class Topic:
+    def __init__(self):
+        self.schedule = None
+        self.topic_str = None
+        self.learning_str = None
+
+    def set_schedule(self, schedule):
+        ''' Set reference back to schedule object parent '''
+        self.schedule = schedule
+
+    def set_topic_learn(self, topic_str, learning_str):
+        self.topic_str = topic_str
+        self.learning_str = learning_str
+
+def test_filling():
+    # Test filling out an object INCOMPLETE
+    topic = Topic()
+    topic.set_topic_learn("1. Introduction to the unit;", "Online class (2 hr)")
+    
+    schedule = Schedule()
+    schedule.set_week("Week 01")
+    schedule.add_topic(topic)
 
     assessment = Assessment()
     assessment.set_weight_desc_body("50%", "Written Final Exam", "It covers all aspects of this unit.")
@@ -65,17 +110,5 @@ class Assessment:
     unit.add_schedule(schedule)
     return unit
 
-
 if __name__ == "__main__":
     unit = test_filling()
-=======
-    assessment = Assessment()
-    assessment.set_weight_desc_body("")
-
-    unit = Unit()
-    unit.set_names("Object Oriented Programming", "INFO1113")
-
-
-if __name__ == "__main__":
-    print("Test")
->>>>>>> Stashed changes
